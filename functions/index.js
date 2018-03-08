c41176e45de398b73ee3af13dd3967490e64851d
Elliot Kirk <elliot.s.kirk@gmail.com>
2018-03-07 16:07:42 -0800

    Added mailing service and firebase triggers on DB write

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+var admin = require('firebase-admin');
+var serviceAccount = require('./clove-a6adf-firebase-adminsdk-91luc-6422ff816a.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Configure email service
+var mailer = nodemailer.createTransport({
+   service: 'Gmail',
+   auth: {
+      user: <Email>,
+      pass: <Password>
+   }
+});
+
+// Set up app to communicate with firebase db
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: 'https://clove-a6adf.firebaseio.com'
+ });
+
+// Send functions to firebase
+exports.newUser = functions.database.ref('users/').onWrite(event => {
+   console.log(event.data.val());
+   
+   // define the mail options
+   let mailOptions = {
+      to: 'getcloveusa@gmail.com',
+      from: 'getcloveusa@gmail.com',
+      subject: 'New Mail List Subscriber',
+      text: 'We just got a new subscriber wooohoooo!'
+   }
+
+   // use nodemailer to send an email to our account
+   mailer.sendMail(mailOptions, (err, info) => {
+      if (err) 
+         console.log(err);
+      console.log('Message sent: %s', info.messageId);
+   });
+
+});
--- public/main.js
+++ public/main.js
@@ -1,27 +1,28 @@
 var Clove = {};
 
 // insert navbar - good for code reusability
-Clove.makeNavbar = function(className) {
+Clove.makeNavbar = function (className) {
    var navbarTemplate = $('#nav_template').html();
-   
-   $('.navbar').addClass('navbar-default navbar-doublerow navbar-trans ' + 
-    'navbar-fixed-top');
+
+   $('.navbar').addClass('navbar-default navbar-doublerow navbar-trans ' +
+      'navbar-fixed-top');
    $('.navbar').append(navbarTemplate);
 }
 
-Clove.addListeners = function() {
+Clove.addListeners = function () {
    // click listeners for modal popup
-   $('.render').click(function(e) {
+   $('.render').click(function (e) {
       $('.lightbox').children().attr('src', $(this).children().attr('src'));
       $('.lightbox').toggleClass('hidden');
       $('.lightbox').removeClass('zoom-out');
       $('.lightbox').toggleClass('zoom');
    });
    // modal listener for shrinking back down
-   $('.lightbox').click(function(e) {
+   $('.lightbox').click(function (e) {
       $('.lightbox').toggleClass('zoom-out');
       $('.lightbox').removeClass('zoom');
-      setTimeout(function(){
+      // 
+      setTimeout(function () {
          $('.lightbox').toggleClass('hidden');
       }, 400);
    });
@@ -32,8 +33,8 @@ Clove.addListeners = function() {
    Clove.addHomePageHandler();
 }
 
-Clove.addPantryPageHandler = function() {
-   $('.nav-pantry').click(function(e) {
+Clove.addPantryPageHandler = function () {
+   $('.nav-pantry').click(function (e) {
       console.log('pantry');
       $('.pantry').removeClass('hidden');
       $('.team').addClass('hidden');
@@ -42,8 +43,8 @@ Clove.addPantryPageHandler = function() {
    });
 }
 
-Clove.addContactPageHandler = function() {
-   $('.nav-contact').click(function(e) {
+Clove.addContactPageHandler = function () {
+   $('.nav-contact').click(function (e) {
       console.log('contact');
       $('.contact').removeClass('hidden');
       $('.team').addClass('hidden');
@@ -52,8 +53,8 @@ Clove.addContactPageHandler = function() {
    });
 }
 
-Clove.addTeamPageHandler = function() {
-   $('.nav-team').click(function(e) {
+Clove.addTeamPageHandler = function () {
+   $('.nav-team').click(function (e) {
       console.log('team');
       $('.team').removeClass('hidden');
       $('.home').addClass('hidden');
@@ -62,14 +63,40 @@ Clove.addTeamPageHandler = function() {
    });
 }
 
-Clove.addHomePageHandler = function() {
-   $('.navbar-brand').click(function(e) {
+Clove.addHomePageHandler = function () {
+   $('.navbar-brand').click(function (e) {
       console.log('home');
       $('.home').removeClass('hidden');
       $('.team').addClass('hidden');
       $('.contact').addClass('hidden');
       $('.pantry').addClass('hidden');
    });
+}
+
+// Firebase config
+var config = {
+   apiKey: 'AIzaSyDgH8_Zxdy6TsW5PcqQmmjZjDBbcyxOAB0',
+   authDomain: 'https://clove-a6adf.firebaseio.com/',
+   databaseURL: 'https://clove-a6adf.firebaseio.com/'
+};
+firebase.initializeApp(config);
+
+var sendEmail = function () {
+   var info = {
+      email: $('.email-field').val(),
+      name: $('.name-field').val(),
+      subscribed: true
+   }
+
+   if (info.name && info.email.indexOf('@') != -1) {
+      firebase.database().ref().child('users/' + info.email.hashCode()).set(info);
+      alert('You are now subscribed to our mailing list!');
+      $('.submit').prop('disabled', true);
+   }
+   else 
+      alert('Incorrect format, try again');
+
+   return false;
 }
 
 // toggle class scroll 
@@ -84,8 +111,19 @@ $(window).scroll(function () {
 
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
    Clove.makeNavbar('navbar');
    Clove.addListeners();
 });
 
+// Turn the email into a hash to be used for the database key
+String.prototype.hashCode = function() {
+   var hash = 0, i, chr;
+   if (this.length === 0) return hash;
+   for (i = 0; i < this.length; i++) {
+     chr   = this.charCodeAt(i);
+     hash  = ((hash << 5) - hash) + chr;
+     hash |= 0; // Convert to 32bit integer
+   }
+   return hash;
+ };
